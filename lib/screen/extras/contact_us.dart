0e479711d55623111741636483071f977335e115
cristianrm13 <cristiangvramos1313@gmail.com>
2024-11-26 10:36:11 -0600

    UPDATE | diseño de contactos

--- lib/screen/extras/contact_us.dart
+++ lib/screen/extras/contact_us.dart
@@ -10,44 +10,164 @@ class ContactUsScreen extends StatelessWidget {
         title: const Text('Contact Us'),
         backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                'Esta aplicaacion fue desarrollada por:',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              Text(
+                'Esta aplicación fue desarrollada por:',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
               ),
-              const SizedBox(height: 20),
-              _buildContactCard('Cristian Gerardo Vázquez Ramos', 'assets/cristian.jpeg'),
-              const SizedBox(height: 10),
-              _buildContactCard('Luis Osvaldo Pérez Ángel', 'assets/luis.jpg'),
-              const SizedBox(height: 10),
-              _buildContactCard('Alan David Balbuena Zavala', 'assets/alan.jpg'),
+              SizedBox(height: 20),
+              AnimatedContactCard(
+                name: 'Cristian Gerardo Vázquez Ramos',
+                imageUrl: 'assets/cristian.jpeg',
+                role: 'Desarrollador FullStack',
+                email: 'cristiangvramos1313@gmail.com',
+                phone: '965 124 8795',
+              ),
+              SizedBox(height: 20),
+              AnimatedContactCard(
+                name: 'Luis Osvaldo Pérez Ángel',
+                imageUrl: 'assets/luis.jpg',
+                role: 'Diseñador UI/UX',
+                email: 'luisOPA@gmail.com',
+                phone: '965 123 6735',
+              ),
+              SizedBox(height: 20),
+              AnimatedContactCard(
+                name: 'Alan David Balbuena Zavala',
+                imageUrl: 'assets/alan.jpg',
+                role: 'Desarrollador Backend',
+                email: 'alanBalbuena@gmail.com',
+                phone: '961 567 8901',
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
 
-  Widget _buildContactCard(String name, String imageUrl) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 40, // Aumenta el tamaño del CircleAvatar
-          backgroundImage: AssetImage(imageUrl), // Cargar la imagen desde assets
+class AnimatedContactCard extends StatefulWidget {
+  final String name;
+  final String imageUrl;
+  final String role;
+  final String email;
+  final String phone;
+
+  const AnimatedContactCard({
+    super.key,
+    required this.name,
+    required this.imageUrl,
+    required this.role,
+    required this.email,
+    required this.phone,
+  });
+
+  @override
+  _AnimatedContactCardState createState() => _AnimatedContactCardState();
+}
+
+class _AnimatedContactCardState extends State<AnimatedContactCard> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _isHovered = true),
+      onTapUp: (_) => setState(() => _isHovered = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: _isHovered ? const Color.fromARGB(255, 208, 247, 211) : Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: _isHovered ? Colors.green.withOpacity(0.5) : Colors.grey.withOpacity(0.3),
+              blurRadius: _isHovered ? 20 : 8,
+              spreadRadius: _isHovered ? 1 : 0,
+              offset: const Offset(0, 3),
+            ),
+          ],
         ),
-        title: Text(name),
-        trailing: const Icon(Icons.message),
-        onTap: () {
-          // Aquí puedes agregar la funcionalidad para enviar un mensaje
-        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                // Puedes usar esta acción para hacer un zoom o redirigir a otro detalle.
+              },
+              child: Transform.scale(
+                scale: _isHovered ? 1.1 : 1.0,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    image: DecorationImage(
+                      image: AssetImage(widget.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              widget.name,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.role,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[700],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.email, color: Colors.green, size: 18),
+                const SizedBox(width: 8),
+                Text(
+                  widget.email,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.phone, color: Colors.green, size: 18),
+                const SizedBox(width: 8),
+                Text(
+                  widget.phone,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/formularioReporte.dart
+++ lib/screen/formularioReporte.dart
@@ -1,324 +1,11 @@
-/* import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-import 'package:shared_preferences/shared_preferences.dart'; 
-
-class FormularioReporte extends StatefulWidget {
-  const FormularioReporte({super.key});
-
-  @override
-  _FormularioReporteState createState() => _FormularioReporteState();
-}
-
-class _FormularioReporteState extends State<FormularioReporte> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  
-  String _categoriaSeleccionada = 'Alumbrado'; // Categoría por defecto
-  File? _selectedImage; // Para almacenar la imagen seleccionada
-  final ImagePicker _picker = ImagePicker(); // Instancia de ImagePicker
-
-  Future<void> _pickImage() async {
-    // Verificar el estado del permiso de almacenamiento
-    var status = await Permission.storage.status;
-    if (!status.isGranted) {
-      // Solicitar permiso si no está concedido
-      await Permission.storage.request();
-    }
-
-    // Seleccionar la imagen
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-
-    if (pickedFile != null) {
-      setState(() {
-        _selectedImage = File(pickedFile.path); // Guardamos la imagen seleccionada
-      });
-    } else {
-      print('No se seleccionó ninguna imagen.');
-    }
-  }
-
-
-/* Future<void> _submitReport() async {
-  final String title = _titleController.text.trim();
-  final String description = _descriptionController.text.trim();
-
-  // Validaciones
-  if (title.isEmpty || _categoriaSeleccionada.isEmpty || description.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Por favor, completa todos los campos y selecciona una imagen.')),
-    );
-    return;
-  }
-
-  // Obtén el token de SharedPreferences
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('token') ?? '';
-
-  if (token.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Error de autenticación. Por favor, inicia sesión de nuevo.')),
-    );
-    Navigator.pushReplacementNamed(context, '/login');
-    return;
-  }
-
-  try {
-    // Envía el reporte con el token en los headers
-    final response = await http.post(
-      Uri.parse('http://gladboxapi.integrador.xyz:3000/api/v1/quejas'),
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer $token", // Incluye el token aquí
-      },
-      body: jsonEncode({
-        'title': title,
-        'description': description,
-        'category': _categoriaSeleccionada,
-      }),
-    );
-
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Reporte enviado exitosamente')),
-      );
-      Navigator.pop(context);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error al enviar el reporte: ${response.body}')),
-      );
-    }
-  } catch (e) {
-    print('Exception: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Error al enviar el reporte.')),
-    );
-  }
-}
- */
-
-Future<void> _submitReport() async {
-  final String title = _titleController.text.trim();
-  final String description = _descriptionController.text.trim();
-
-  if (title.isEmpty || _categoriaSeleccionada.isEmpty || description.isEmpty || _selectedImage == null) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Por favor, completa todos los campos y selecciona una imagen.')),
-    );
-    return;
-  }
-
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('token') ?? '';
-
-  if (token.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Error de autenticación. Por favor, inicia sesión de nuevo.')),
-    );
-    Navigator.pushReplacementNamed(context, '/login');
-    return;
-  }
-
-  try {
-    final request = http.MultipartRequest(
-      'POST',
-      Uri.parse('http://gladboxapi.integrador.xyz:3000/api/v1/quejas'),
-    );
-
-    request.headers.addAll({
-      "Authorization": "Bearer $token",
-    });
-
-    request.fields['title'] = title;
-    request.fields['description'] = description;
-    request.fields['category'] = _categoriaSeleccionada;
-
-    // Adjunta la imagen al cuerpo de la solicitud
-    request.files.add(
-      await http.MultipartFile.fromPath(
-        'image', // Nombre del campo esperado en el backend
-        _selectedImage!.path,
-      ),
-    );
-
-    final response = await request.send();
-
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Reporte enviado exitosamente')),
-      );
-      Navigator.pop(context);
-    } else {
-      final responseBody = await response.stream.bytesToString();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error al enviar el reporte: $responseBody')),
-      );
-    }
-  } catch (e) {
-    print('Exception: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Error al enviar el reporte.')),
-    );
-  }
-}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context); // Regresar a la pantalla anterior
-          },
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text(
-          'Crear reporte',
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('assets/Gladbox.png', width: 40),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Título',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _titleController,
-                decoration: InputDecoration(
-                  hintText: '...',
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Categoría',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              DropdownButtonFormField<String>(
-                value: _categoriaSeleccionada,
-                items: <String>['Alumbrado', 'Baches', 'Limpieza', 'Seguridad']
-                    .map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _categoriaSeleccionada = newValue!;
-                  });
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Descripción',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _descriptionController,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  hintText: 'Descripción...',
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Subir evidencias',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: _pickImage, // Método para seleccionar la imagen
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  child: _selectedImage != null
-                      ? Image.file(
-                          _selectedImage!,
-                          fit: BoxFit.cover,
-                        )
-                      : const Center(
-                          child: Text('Tap para seleccionar una imagen'),
-                        ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              Center(
-                child: ElevatedButton(
-                  onPressed: _submitReport, // Llama a la función que envía el reporte
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 80, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'Reportar',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-} */
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:audioplayers/audioplayers.dart'; // Importa audioplayers
+
 
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -336,21 +23,19 @@ class _FormularioReporteState extends State<FormularioReporte> {
   String _categoriaSeleccionada = 'alumbrado'; // Categoría por defecto
   File? _selectedImage; // Para almacenar la imagen seleccionada
   final ImagePicker _picker = ImagePicker(); // Instancia de ImagePicker
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   Future<void> _pickImage() async {
-    // Verificar el estado del permiso de almacenamiento
     var status = await Permission.storage.status;
     if (!status.isGranted) {
-      // Solicitar permiso si no está concedido
       await Permission.storage.request();
     }
 
-    // Seleccionar la imagen
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
       setState(() {
-        _selectedImage = File(pickedFile.path); // Guardamos la imagen seleccionada
+        _selectedImage = File(pickedFile.path); 
       });
     } else {
       print('No se seleccionó ninguna imagen.');
@@ -405,6 +90,7 @@ class _FormularioReporteState extends State<FormularioReporte> {
 
       if (response.statusCode == 200 || response.statusCode == 201) {
         // Mostrar el diálogo de confirmación
+        //_playSuccessSound();
         _showConfirmationDialog();
       } else {
         final responseBody = await response.stream.bytesToString();
@@ -419,6 +105,14 @@ class _FormularioReporteState extends State<FormularioReporte> {
       );
     }
   }
+
+/*     Future<void> _playSuccessSound() async {
+    try {
+      await _audioPlayer.play(AssetSource('assets/sounds/sent.wav')); // Reproducir sonido desde assets
+    } catch (e) {
+      print('Error al reproducir el sonido: $e');
+    }
+  } */
 
   void _showConfirmationDialog() {
     showDialog(
@@ -589,3 +283,6 @@ class _FormularioReporteState extends State<FormularioReporte> {
     );
   }
 }
+
+
+
